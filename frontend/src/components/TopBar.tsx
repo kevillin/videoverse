fcf3deaded0c06364882a1778cb4d10374863f5d
pllsg96 <pedroluizlsg@gmail.com>
2023-07-18 19:33:52 -0300

    add: config browser router

---
+++ frontend/src/components/TopBar.tsx
@@ -0,0 +1,18 @@
+// import { useState } from 'react'
+import './TopBar.css'
+
+function TopBar() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='TopBarColor'>
+        <h1>TopBar</h1>
+        <h1>TopBar</h1>
+        <h1>TopBar</h1>
+      </div>
+    </>
+  )
+}
+
+export default TopBar
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,18 @@
+// import { useState } from 'react'
+import TopBar from '../components/TopBar'
+import './Home.css'
+
+function Home() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <TopBar />
+        <h1>Home</h1>
+      </div>
+    </>
+  )
+}
+
+export default Home
